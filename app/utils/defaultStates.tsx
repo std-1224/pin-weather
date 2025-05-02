90eed5b3fee59ce7cf7b4735bb357f63da62bb47
std-1224 <kalkaudy@gmail.com>
2025-05-02 23:35:53 +0700

    updatd the store

---
+++ app/utils/defaultStates.tsx
@@ -0,0 +1,25 @@
+const defaultStates = [
+  {
+    name: "Singapore",
+    country: "SG",
+    state: "Singapore",
+    lat: 1.2899175,
+    lon: 103.8519072,
+  },
+  {
+    name: "Yangon",
+    country: "MM",
+    state: "Myanmar",
+    lat: 16.7967129,
+    lon: 96.1609916,
+  },
+  {
+    name: "Bangkok",
+    country: "TH",
+    state: "Thailand",
+    lat: 13.7524938,
+    lon: 100.4935089,
+  },
+];
+
+export default defaultStates;
